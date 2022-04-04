c6d37c6d9baf01a19c72157aa92e1a51e7d4d35c
Egor_Babcinetchi <egor.babcinetchi@isa.utm.md>
2022-04-04 21:13:30 +0300

    minor changes

--- src/main/java/com/example/educational/dto/PersonProfileCreateDTO.java
+++ src/main/java/com/example/educational/dto/PersonProfileCreateDTO.java
@@ -1,7 +1,14 @@
 package com.example.educational.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@AllArgsConstructor
+@RequiredArgsConstructor
 public class PersonProfileCreateDTO {
     private Integer age;
     private String name;
